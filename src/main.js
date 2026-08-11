8583c5242796d1fc4ca6776d96d38b5ec44e07a1
Kolya Kolchenko <kk.frl.webdev@gmail.com>
2026-08-11 15:48:17 +0300

    Fix lint error blocking deployment

--- src/main.js
+++ src/main.js
@@ -1,9 +1,8 @@
-import { createApp } from "vue";
-import { createPinia } from "pinia";
-import App from "./App.vue";
-import router from "./router";
-import { i18n } from "./i18n";
-import "./assets/styles/main.scss";
+import { createApp } from 'vue'
+import { createPinia } from 'pinia'
+import App from './App.vue'
+import router from './router'
+import { i18n } from './i18n'
+import './assets/styles/main.scss'
 
-createApp(App).use(createPinia()).use(i18n).use(router).mount("#app");
-І
+createApp(App).use(createPinia()).use(i18n).use(router).mount('#app')
